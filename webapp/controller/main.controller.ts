dc679130af48858c2a36b651a6893f0a7c9a36da
Gert Mertens <gertmertens92@gmail.com>
2023-08-22 12:08:07 +0000

    Initial

---
+++ webapp/controller/main.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace be.flexso.cloudgovernance.controller
+ */
+export default class main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
